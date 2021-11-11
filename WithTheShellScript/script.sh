1bd01dfc2a74c1b829492c3fa8cd84aee80f8dd6
sipe-daniel <sipetchawoudanieljordan@yahoo.fr>
2021-11-11 16:49:07 +0100

    refactor WithTheShellScript

--- WithTheShellScript/script.sh
+++ WithTheShellScript/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl https://www.meteocity.com/france/lille-v2998324 | grep '<meta property="og:title"' | cut -d '"' -f 4 > result.txt
+curl https://www.meteocity.com/france/lille-v2998324 | grep 'property="og:title"' | sed "s/.* content=\"\(.*\)\".*/\1/" > result.txt
+
 
 
